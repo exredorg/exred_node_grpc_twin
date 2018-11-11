bf8337ac53a4cf7f73bfd78cc9425e4b424d6e42
Zsolt <zsoltxyz@gmail.com>
2018-11-11 15:54:14 -0500

    bumped version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exred.Node.GrpcTwin.MixProject do
   def project do
     [
       app: :exred_node_grpc_twin,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
