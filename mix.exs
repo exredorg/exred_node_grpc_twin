01cc0666dea126bc804f1867f9c76dad8bb076b7
Zsolt <zsoltxyz@gmail.com>
2018-11-11 16:42:07 -0500

    updated mix.exs with package data

--- mix.exs
+++ mix.exs
@@ -1,12 +1,16 @@
 defmodule Exred.Node.GrpcTwin.MixProject do
   use Mix.Project
 
+  @description "Exred node that pairs with an external service"
+
   def project do
     [
       app: :exred_node_grpc_twin,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      description: @description,
+      package: package(),
       deps: deps()
     ]
   end
@@ -24,5 +28,17 @@ defmodule Exred.Node.GrpcTwin.MixProject do
       {:ex_doc, "~> 0.19.0", only: :dev, runtime: false},
       {:exred_library, "~> 0.1"}
     ]
+  end
+
+  defp package do
+    %{
+      licenses: ["MIT"],
+      maintainers: ["Zsolt Keszthelyi"],
+      links: %{
+        "GitHub" => "https://github.com/exredorg/exred_node_grpc_twin",
+        "Exred" => "http://exred.org"
+      },
+      files: ["lib", "mix.exs", "README.md", "LICENSE"]
+    }
   end
 end
